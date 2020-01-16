ee644ea0e10c848fce1034d703c58ac0625f9669
yuvallevy <yuvallevy3594@gmail.com>
2020-01-17 00:00:48 +0200

    17.01.20 00:00

--- MatrixSolver.cpp
+++ MatrixSolver.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "MatrixSolver.h"
+using namespace std;
+
+MatrixSolver::MatrixSolver(Searcher<Point> *myAlgorithmSearcher) : myAlgorithmSearcher(myAlgorithmSearcher) {
+
+}
+
+string MatrixSolver::solve(Searchable<Point> *problem) {
+    vector<State<Point>* > myPath;
+    myPath = this->myAlgorithmSearcher->search(problem);
+    return returnPath(myPath);
+}
+
+string MatrixSolver::returnPath(vector<State<Point> *> myPath) {
+    if(myPath.empty()){
+        return "-1";
+    }
+    string myStringSolution;
+    int sizeOfPath = myPath.size();
+    for(int i=0; i < sizeOfPath; i++){
+        myStringSolution.append(returnNextMove(myPath[i]->getState(), myPath[i+1]->getState()));
+        myStringSolution.append(",");
+    }
+    return myStringSolution;
+}
+
+// return the next move in string.
+string MatrixSolver::returnNextMove(Point prevPoint, Point thisPoint) {
+    int prevLeft = prevPoint.getLeft();
+    int prevRight = prevPoint.getRight();
+    int currLeft = thisPoint.getLeft();
+    int currRight = thisPoint.getRight();
+    if(prevLeft < currLeft){
+        return "DOWN";
+    } else if (prevLeft > currLeft){
+        return "UP";
+    } else if (prevRight < currRight){
+        return "RIGHT";
+    } else{
+        return "LEFT";
+    }
+}
+
--- MatrixSolver.h
+++ MatrixSolver.h
@@ -8,10 +8,20 @@
 #include "Solver.h"
 #include "Searchable.h"
 #include "Searcher.h"
-
+#pragma once
+using namespace std;
 class MatrixSolver : public Solver<Searchable<Point>*, string> {
+private:
+    Searcher<Point>* myAlgorithmSearcher;
+    string returnPath(vector<State<Point>* > myPath);
+    string returnNextMove(Point prevPoint ,Point thisPoint);
 
-};
+public:
+    string solve(Searchable<Point> *problem) override;
+
+    MatrixSolver(Searcher<Point> *myAlgorithmSearcher);
+
+}
 
 
 #endif //EX4_MATRIXSOLVER_H
